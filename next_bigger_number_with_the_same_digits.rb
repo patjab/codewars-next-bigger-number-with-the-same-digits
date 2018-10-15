84dbd447ac87b4e691df7c5d6f226726f9c7e2cb
github username <wireless.patrick@gmail.com>
2018-10-15 11:20:19 -0400

    refactored

--- next_bigger_number_with_the_same_digits.rb
+++ next_bigger_number_with_the_same_digits.rb
@@ -1,31 +1,23 @@
 def next_bigger(n)
   split_digits = n.to_s.split("").map{ |digit| digit.to_i }
-  sorted_digits = split_digits.sort { |x, y| y - x }
+  pivot = -2
   
-  if split_digits == sorted_digits
+  if split_digits == split_digits.sort { |x, y| y - x }
     return -1
   end
   
-  original_split_digits = split_digits.dup
-  tracker_left = -2
-    
-  while split_digits[tracker_left] >= split_digits[tracker_left + 1] 
-    tracker_left -= 1 
+  while split_digits[pivot] >= split_digits[pivot + 1] 
+    pivot -= 1 
   end 
-  
-  desc_end_sect = split_digits[tracker_left..-1].sort {|x, y| x - y}.dup
-  index_of_pivot = desc_end_sect.index(split_digits[tracker_left])
+    
+  desc_end_sect = split_digits[pivot..-1].sort {|x, y| x - y}  
+  index_of_pivot = desc_end_sect.index(split_digits[pivot])
   
   i = 0
-  next_num_to_pivot = desc_end_sect[index_of_pivot + i]
-  loop do
+  while desc_end_sect[index_of_pivot + i] == split_digits[pivot]
     i += 1
-    next_num_to_pivot = desc_end_sect[index_of_pivot + i]
-    break if split_digits[tracker_left] != next_num_to_pivot
   end
+  next_num_to_pivot = desc_end_sect[index_of_pivot + i]
   
-  desc_end_sect.delete_at(index_of_pivot + i)
-  
-  split_digits = (split_digits[0...tracker_left] + [next_num_to_pivot] + desc_end_sect)
-  split_digits != original_split_digits ? split_digits.join.to_i : -1
+  (split_digits[0...pivot] + [desc_end_sect.delete_at(index_of_pivot + i)]  + desc_end_sect).join.to_i
 end
